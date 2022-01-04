cfed3735720aad8a3eed2e8925e46dd7d1eda820
s-bhatla <sidroot02@gmail.com>
2022-01-05 00:31:58 +0530

    added timeline

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,31 +1,59 @@
-import "./styles/Counter.css"
+import "./styles/Counter.css";
+import CountUp from "react-countup";
+import VisibilitySensor from "react-visibility-sensor";
 
-function Counter(){
-    return (
-        <div id="counter" className="d-flex justify-content-around">
-            {/* Add counting effect and paralax */}
-            <div className="d-flex flex-column justify-content-center counter-ele">
-                <i class="far fa-lightbulb"></i>
-                <div>69</div>
-                <div className="d-flex justify-content-center">Completed Projects</div>
-            </div>
-            <div className="d-flex flex-column justify-content-center counter-ele">
-                <i class="fas fa-laptop-code"></i>
-                <div>69</div>
-                <div>Hackathons Participated</div>
-            </div>
-            <div className="d-flex flex-column justify-content-center counter-ele">
-                <i class="fas fa-award"></i>
-                <div>69</div>
-                <div>Awards</div>
-            </div>
-            <div className="d-flex flex-column justify-content-center counter-ele">
-                <i class="fas fa-user-plus"></i>
-                <div>69</div>
-                <div>LinkedIn Connections</div>
-            </div>
+function Counter() {
+  return (
+    <div id="counter" className="d-flex justify-content-around">
+      {/* Add counting effect and paralax */}
+      <div className="d-flex flex-column justify-content-center counter-ele">
+        <i class="far fa-lightbulb"></i>
+        <div>
+          <VisibilitySensor partialVisibility offset={{ bottom: 100 }}>
+            {({ isVisible }) => (
+              <div>
+                {isVisible ? <CountUp end={5} duration={2} /> : 0}
+              </div>
+            )}
+          </VisibilitySensor>
         </div>
-    )
+        <div className="d-flex justify-content-center">Completed Projects</div>
+      </div>
+      <div className="d-flex flex-column justify-content-center counter-ele">
+        <i class="fas fa-laptop-code"></i>
+        <div><VisibilitySensor partialVisibility offset={{ bottom: 100 }}>
+            {({ isVisible }) => (
+              <div>
+                {isVisible ? <CountUp end={8} duration={2} /> : 0}
+              </div>
+            )}
+          </VisibilitySensor></div>
+        <div>Hackathons Participated</div>
+      </div>
+      <div className="d-flex flex-column justify-content-center counter-ele">
+        <i class="fas fa-award"></i>
+        <div><VisibilitySensor partialVisibility offset={{ bottom: 100 }}>
+            {({ isVisible }) => (
+              <div>
+                {isVisible ? <CountUp end={1} duration={2} /> : 0}
+              </div>
+            )}
+          </VisibilitySensor></div>
+        <div>Awards</div>
+      </div>
+      <div className="d-flex flex-column justify-content-center counter-ele">
+        <i class="fas fa-user-plus"></i>
+        <div><VisibilitySensor partialVisibility offset={{ bottom: 100 }}>
+            {({ isVisible }) => (
+              <div>
+                {isVisible ? <CountUp end={166} duration={2} /> : 169}
+              </div>
+            )}
+          </VisibilitySensor></div>
+        <div>LinkedIn Connections</div>
+      </div>
+    </div>
+  );
 }
 
 export default Counter;
--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,12 +1,86 @@
-function Experience(){
-    return (
-        <div id="experience">
-            Experience
-            {/* Create a timeline here */}
-            <div className="education"></div>
-            <div className="experience"></div>
-        </div>
-    )
+import "./styles/Experience.css";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import { ReactComponent as WorkIcon } from "./work.svg";
+import { ReactComponent as SchoolIcon } from "./school.svg";
+
+function Experience() {
+  return (
+    <div id="experience" className="container-fluid">
+      <h2>Experience</h2>
+      {/* Create a timeline here */}
+      {/* #b46fd9 */}
+      <VerticalTimeline lineColor="#b46fd9">
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="2002-2018"
+          contentStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+          iconStyle={{ background: "rgb(233, 30, 99)", color: "#fff" }}
+          contentArrowStyle={{ borderRight: "7px solid  rgb(33, 150, 243)" }}
+          icon={<SchoolIcon />}
+          position={"left"}
+        >
+          <h3 className="vertical-timeline-element-title">
+            Ryan International School,
+          </h3>
+          <h4 className="vertical-timeline-element-subtitle">
+            Chandigarh
+          </h4>
+          <p>Class 10th</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="2020-present"
+          contentStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+          iconStyle={{ background: "rgb(233, 30, 99)", color: "#fff" }}
+          contentArrowStyle={{ borderRight: "7px solid  rgb(33, 150, 243)" }}
+          icon={<SchoolIcon />}
+          position={"left"}
+        >
+          <h3 className="vertical-timeline-element-title">
+            National Institute of Technology,
+          </h3>
+          <h4 className="vertical-timeline-element-subtitle">
+            Delhi
+          </h4>
+          <p>Bachelor of Technology in Computer Science and Engineering</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+          contentArrowStyle={{ borderRight: "7px solid  rgb(33, 150, 243)" }}
+          date="2020 - present"
+          iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+          icon={<WorkIcon />}
+          position={"right"}
+        >
+          <h3 className="vertical-timeline-element-title">Volunteer</h3>
+          <h4 className="vertical-timeline-element-subtitle">NIT-Delhi Technical Club, Upvision</h4>
+          <p>
+            Worked to help organize various multiple tournaments for the college community
+          </p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+          contentArrowStyle={{ borderRight: "7px solid  rgb(33, 150, 243)" }}
+          date="2020 - present"
+          iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+          icon={<WorkIcon />}
+          position={"right"}
+        >
+          <h3 className="vertical-timeline-element-title">Executive Member</h3>
+          <h4 className="vertical-timeline-element-subtitle">Google Developer Student Club</h4>
+          <p>
+            Worked to organize various seminars and events which were attended by over 150 students
+          </p>
+        </VerticalTimelineElement>
+      </VerticalTimeline>
+    </div>
+  );
 }
 
 export default Experience;
