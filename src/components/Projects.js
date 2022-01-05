6f3070aff168111508c45d2286657020041fa0da
s-bhatla <sidroot02@gmail.com>
2022-01-05 21:05:54 +0530

    added hover effect

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,83 +1,136 @@
 import "./styles/Projects.css";
 import { useState } from "react";
 
-
 function Projects() {
-
   const [showproj, setShowproj] = useState("nothing");
 
   const display_web = () => {
-    setShowproj("display-web")
-  }
+    setShowproj("display-web");
+  };
   const display_android = () => {
-    setShowproj("display-android")
-  }
+    setShowproj("display-android");
+  };
   const display_others = () => {
-    setShowproj("display-others")
-  }
+    setShowproj("display-others");
+  };
   const display_all = () => {
-    setShowproj("")
-  }
-
+    setShowproj("");
+  };
 
   return (
     <div id="projects" className="container-fluid">
       <h2>Projects</h2>
       <div className="d-flex mb-3">
-        <div className="p-2 sorters" onClick={display_all}>All</div>
-        <div className="p-2 sorters" onClick={display_web}>Web</div>
-        <div className="p-2 sorters" onClick={display_android}>Android</div>
-        <div className="p-2 sorters" onClick={display_others}>Others</div>
+        <div className="p-2 sorters" onClick={display_all}>
+          All
+        </div>
+        <div className="p-2 sorters" onClick={display_web}>
+          Web
+        </div>
+        <div className="p-2 sorters" onClick={display_android}>
+          Android
+        </div>
+        <div className="p-2 sorters" onClick={display_others}>
+          Others
+        </div>
       </div>
-      <div className="row cards-list">
-        <div className={`col-md-4 col-sm-6 p-4 android ${showproj}`}>
-          <div>
+      <div className="row cards-list wrapper">
+        <div className={`col-md-4 col-sm-6 android ${showproj}`}>
+          <div className="mycard m-4">
             <img
               src={`${process.env.PUBLIC_URL}/assets/vaxiqueue.png`}
               alt="VaxiQueue"
               height="200vh"
             />
+            <div className="card-info">
+              <h3>Vaxiqueue</h3>
+              <p>
+                An android app to predict the waiting time for people waiting to
+                get a vaccine.
+              </p>
+              <a href="https://github.com/s-bhatla/VaxiQueue" class="btn">
+                Go to Project
+              </a>
+            </div>
           </div>
         </div>
-        <div className={`col-md-4 col-sm-6 p-4 others ${showproj}`}>
-          <div>
+        <div className={`col-md-4 col-sm-6 others ${showproj}`}>
+          <div className="mycard m-4">
             <img
               src={`${process.env.PUBLIC_URL}/assets/minichess.png`}
               alt="Chess Mini Game"
               height="200vh"
             />
+            <div className="card-info">
+              <h3>Vaxiqueue</h3>
+              <p>
+                An android app to predict the waiting time for people waiting to
+                get a vaccine.
+              </p>
+              <a href="https://github.com/s-bhatla/VaxiQueue" class="btn">
+                Go to Project
+              </a>
+            </div>
           </div>
         </div>
-        <div className={`col-md-4 col-sm-6 p-4 android ${showproj}`}>
-          <div>
+        <div className={`col-md-4 col-sm-6 android ${showproj}`}>
+          <div className="m-4 mycard">
             <img
               src={`${process.env.PUBLIC_URL}/assets/chromavision.jpg`}
               alt="ChromaVision"
               height="200vh"
             />
+            <div className="card-info">
+              <h3>Vaxiqueue</h3>
+              <p>
+                An android app to predict the waiting time for people waiting to
+                get a vaccine.
+              </p>
+              <a href="https://github.com/s-bhatla/VaxiQueue" class="btn">
+                Go to Project
+              </a>
+            </div>
           </div>
         </div>
-        <div className={`col-md-4 col-sm-6 p-4 web ${showproj}`}>
-          <div>
+        <div className={`col-md-4 col-sm-6 web ${showproj}`}>
+          <div className="m-4 mycard">
             <img
               src={`${process.env.PUBLIC_URL}/assets/swiftpharma.png`}
               alt="SwiftPharma"
               height="200vh"
             />
+            <div className="card-info">
+              <h3>Vaxiqueue</h3>
+              <p>
+                An android app to predict the waiting time for people waiting to
+                get a vaccine.
+              </p>
+              <a href="https://github.com/s-bhatla/VaxiQueue" class="btn">
+                Go to Project
+              </a>
+            </div>
           </div>
         </div>
-        <div className={`col-md-4 col-sm-6 p-4 web ${showproj}`}>
-          <div>
+        <div className={`col-md-4 col-sm-6 web ${showproj}`}>
+          <div className="m-4 mycard">
             <img
               src={`${process.env.PUBLIC_URL}/assets/portfolio.jpg`}
               alt="This Portfolio Website"
               height="200vh"
             />
+            <div className="card-info">
+              <h3>Vaxiqueue</h3>
+              <p>
+                An android app to predict the waiting time for people waiting to
+                get a vaccine.
+              </p>
+              <a href="https://github.com/s-bhatla/VaxiQueue" class="btn">
+                Go to Project
+              </a>
+            </div>
           </div>
         </div>
       </div>
-      {/* Render Project Cards list here */}
-      {/* Also Get Images for those projects in the folder */}
     </div>
   );
 }
