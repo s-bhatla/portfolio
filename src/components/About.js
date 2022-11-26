94db859ff82e77f10f2a6e63ecd9311b66a5cf7f
s-bhatla <sidroot02@gmail.com>
2022-11-26 20:42:27 +0530

    added back animation & improved glow

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,13 @@
 import myImg from "./myImg.webp";
 import "./styles/About.css";
-// import Aos from "aos";
-// import "aos/dist/aos.css";
-// import { useEffect } from "react";
+import Aos from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 
 function About() {
-  // useEffect(() => {
-  //   Aos.init({ duration: 2000 });
-  // }, []);
+  useEffect(() => {
+    Aos.init({ duration: 2000 });
+  }, []);
 
   return (
     <div id="about" className="container-fluid">
@@ -15,16 +15,16 @@ function About() {
       <div className="row">
         <img
           loading="lazy"
-          // data-aos="fade-right"
-          // data-aos-once="true"
+          data-aos="fade-right"
+          data-aos-once="true"
           src={myImg}
           alt="I, in the flesh."
           className="col-lg-4 image"
         />
         <div
           className="col-lg-8 d-flex justify-content-center flex-column summary"
-          // data-aos-once="true"
-          // data-aos="fade-up"
+          data-aos-once="true"
+          data-aos="fade-up"
         >
           <div className="d-flex justify-content-center flex-column">
             <div className="mb-2">
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,24 +1,24 @@
 import "./styles/Contact.css";
 import mailbox from "./mail-box.png";
-// import Aos from "aos";
-// import "aos/dist/aos.css"
-// import { useEffect } from "react";
+import Aos from "aos";
+import "aos/dist/aos.css"
+import { useEffect } from "react";
 
 
 function Contact() {
-  // useEffect(() => {
-  //   Aos.init({duration:2000})
-  // }, [])
+  useEffect(() => {
+    Aos.init({duration:2000})
+  }, [])
 
   return (
     <div id="contact" class="container-fluid">
       <h2 className="topic-head">Get in Touch</h2>
       <div className="d-flex justify-content-center mt-3 contact-text">
         <img loading="lazy" src={mailbox} alt="mailbox." className=" mail-img" 
-        // data-aos="fade-right" data-aos-once="true" 
+        data-aos="fade-right" data-aos-once="true" 
         />
         <div className="contact-text"  
-        // data-aos="fade-up" data-aos-once="true"
+        data-aos="fade-up" data-aos-once="true"
         >
           Feel free to drop an email, or contact me on any of the social
           platforms provided. I'm always interested in new and exciting
